d82907d9f7f8183942b47643ee55821696220771
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 17:11:25 -0400

    Header Component
    -Created the header component and filled it with subcomponents Logo and Currency

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import Logo from '../Logo/Logo';
+import Currency from '../Currency/Currency';
+
+export default function Header() {
+    return (
+       <>
+        <Logo/>
+        Title
+        <Currency/>
+       </>
+    );
+}
---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import {React} from "react";
+
+export default function Logo() {
+    return (
+        <div className="logo">
+            <img src="https://via.placeholder.com/150" alt="Logo"/>
+        </div>
+    );
+}
