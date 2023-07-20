486edf7080ae4bfb5564ad544def8addb7c49d76
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 18:24:51 -0400

    Upgrades Spacing

--- frontend/src/components/Upgrade/Upgrade.jsx
+++ frontend/src/components/Upgrade/Upgrade.jsx
@@ -1,12 +1,17 @@
-import {React} from 'react';
-import PurchaseButton from '../PurchaseButton/PurchaseButton';
-import UpgradePhoto from '../UpgradePhoto/UpgradePhoto';
+import { React } from "react";
+import "./Upgrade.css";
+import PurchaseButton from "../PurchaseButton/PurchaseButton";
+import UpgradePhoto from "../UpgradePhoto/UpgradePhoto";
 
 export default function Upgrade() {
-    return (
-        <div className="upgrade">
-            <UpgradePhoto/>
-            <PurchaseButton/>
-        </div>
-    );
-};
+  return (
+    <div className="upgrade">
+      <UpgradePhoto />
+      <div className="upgrade-container">
+        <h3>Upgrade Name</h3>
+        <p>Upgrade Description</p>
+      </div>
+      <PurchaseButton />
+    </div>
+  );
+}
--- frontend/src/components/Upgrades/Upgrades.jsx
+++ frontend/src/components/Upgrades/Upgrades.jsx
@@ -1,10 +1,16 @@
 import {React} from 'react';
+import './Upgrades.css';
 import Upgrade from '../Upgrade/Upgrade';
 
 
 export default function Upgrades() {
     return (
-        <div className="Upgrades">
+        <div className="upgrades">
+            <Upgrade></Upgrade>
+            <Upgrade></Upgrade>
+            <Upgrade></Upgrade>
+            <Upgrade></Upgrade>
+            <Upgrade></Upgrade>
             <Upgrade></Upgrade>
             <Upgrade></Upgrade>
             <Upgrade></Upgrade>
