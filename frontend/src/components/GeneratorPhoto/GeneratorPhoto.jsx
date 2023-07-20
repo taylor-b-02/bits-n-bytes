cb127993aaf1197f6d473ef58f1dd9174c65b23b
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 17:18:32 -0400

    Generator Component
    -Added generator component and the following subcomponent: generator > generatorPhoto, purchaseButton

---
+++ frontend/src/components/GeneratorPhoto/GeneratorPhoto.jsx
@@ -0,0 +1,10 @@
+import {React} from 'react';
+
+
+export default function GeneratorPhoto() {
+    return (
+        <div className="generator-photo">
+            <img src="https://via.placeholder.com/150" alt="generator"/>
+        </div>
+    );
+};
---
+++ frontend/src/components/Generators/Generators.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import Generator from '../Generator/Generator';
+
+export default function Generators() {
+    return (
+        <div className="generators">
+            <Generator></Generator>
+            <Generator></Generator>
+            <Generator></Generator>
+        </div>
+    );
+};
