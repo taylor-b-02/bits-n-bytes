d65ab778d802d6f898d415ddfedf45eecc8fdc8e
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 18:57:40 -0400

    Modal Component Added

--- frontend/src/components/Page/Page.jsx
+++ frontend/src/components/Page/Page.jsx
@@ -1,17 +1,52 @@
-import {React} from 'react';
-import './Page.css';
-import Header from '../Header/Header';
-import Generators from '../Generators/Generators';
-import Clicker from '../Clicker/Clicker';
-import Upgrades from '../Upgrades/Upgrades';
+import { React, useState } from "react";
+import "./Page.css";
+import Header from "../Header/Header";
+import Generators from "../Generators/Generators";
+import Clicker from "../Clicker/Clicker";
+import Upgrades from "../Upgrades/Upgrades";
+
+//Modal imports
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 export default function Page() {
-    return (
-        <div className="page">
-            <Header/>
-            <Generators/>
-            <Clicker/>
-            <Upgrades/>
-        </div>
-    );
-};
+  let subtitle;
+  const [modalIsOpen, modalSetIsOpen] = useState(true);
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    modalSetIsOpen(false);
+  }
+  return (
+    <div className="page">
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <form>
+          <label>Username </label>
+          <input />
+          <br />
+          <label>Password </label>
+          <input />
+          <br />
+          <button>Submit</button>
+        </form>
+      </Modal>
+      <Header />
+      <Generators />
+      <Clicker />
+      <Upgrades />
+    </div>
+  );
+}
