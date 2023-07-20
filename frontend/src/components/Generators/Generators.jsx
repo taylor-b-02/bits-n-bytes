9a86586abec06dc628b27e13266d4212ca6bedca
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 18:07:19 -0400

    Generator(s) styling

--- frontend/src/components/Generators/Generators.jsx
+++ frontend/src/components/Generators/Generators.jsx
@@ -1,9 +1,16 @@
 import {React} from 'react';
+import './Generators.css';
 import Generator from '../Generator/Generator';
 
 export default function Generators() {
     return (
         <div className="generators">
+            <Generator></Generator>
+            <Generator></Generator>
+            <Generator></Generator>
+            <Generator></Generator>
+            <Generator></Generator>
+            <Generator></Generator>
             <Generator></Generator>
             <Generator></Generator>
             <Generator></Generator>
--- frontend/src/components/PurchaseButton/PurchaseButton.jsx
+++ frontend/src/components/PurchaseButton/PurchaseButton.jsx
@@ -1,4 +1,5 @@
 import {React} from 'react';
+import './PurchaseButton.css';
 
 export default function PurchaseButton() {
     return (
