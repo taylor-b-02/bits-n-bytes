d220ca3c81353f515fab22f8c4348d1f7279ad41
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 17:21:08 -0400

    Upgrades Component
    -Added the upgrades component along with the following subcomponents: Upgrade > UpgradePhoto, PurchaseButton

---
+++ frontend/src/components/Upgrade/Upgrade.jsx
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import PurchaseButton from '../PurchaseButton/PurchaseButton';
+import UpgradePhoto from '../UpgradePhoto/UpgradePhoto';
+
+export default function Upgrade() {
+    return (
+        <div className="upgrade">
+            <UpgradePhoto/>
+            I am an upgrade
+            <PurchaseButton/>
+        </div>
+    );
+};
