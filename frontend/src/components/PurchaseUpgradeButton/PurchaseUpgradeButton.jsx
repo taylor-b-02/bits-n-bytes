59834c1e4d16b933db5b6bccdab810c45780d0db
Maguire McMahon <mimcmahon20@vt.edu>
2023-07-20 21:03:50 -0400

    Added purchaseUpgradeButton component

---
+++ frontend/src/components/PurchaseUpgradeButton/PurchaseUpgradeButton.jsx
@@ -0,0 +1,10 @@
+import {React} from 'react';
+import './PurchaseUpgradeButton.css';
+
+export default function PurchaseButton() {
+    return (
+        <div className="purchase-button">
+            <button>Purchase</button>
+        </div>
+    );
+};
