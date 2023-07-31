52991bd8e6c612abaa1933da78e0a44a269a0080
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-31 18:22:29 -0400

    Styling upgrade button

--- frontend/src/components/PurchaseUpgradeButton/PurchaseUpgradeButton.jsx
+++ frontend/src/components/PurchaseUpgradeButton/PurchaseUpgradeButton.jsx
@@ -7,9 +7,16 @@ export default function PurchaseButton(props) {
     function handleClick() {
         onUpgradeBought(upgrade);
     }
+
+    function generateCost() {
+        const number = Math.floor(cost);
+        const formattedNumber = number.toLocaleString("en-US");
+        return formattedNumber;
+      }
+
     return (
         <div className="purchase-button">
-            <button onClick={handleClick} className='upgrade-button'>{Math.floor(cost)}</button>
+            <button onClick={handleClick} className='upgrade-button'>{generateCost()}</button>
         </div>
     );
 };
