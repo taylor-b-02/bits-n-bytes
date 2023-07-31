c58ab2dcfccf1bce476a95bef6ee619a4f9e657c
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-31 17:18:23 -0400

    functional upgrades, need to decide direction now

--- frontend/src/components/PurchaseUpgradeButton/PurchaseUpgradeButton.jsx
+++ frontend/src/components/PurchaseUpgradeButton/PurchaseUpgradeButton.jsx
@@ -1,10 +1,15 @@
 import {React} from 'react';
 import './PurchaseUpgradeButton.css';
 
-export default function PurchaseButton() {
+export default function PurchaseButton(props) {
+    const {upgrade, cost, onUpgradeBought} = props;
+
+    function handleClick() {
+        onUpgradeBought(upgrade);
+    }
     return (
         <div className="purchase-button">
-            <button>Purchase</button>
+            <button onClick={handleClick}>{Math.floor(cost)}</button>
         </div>
     );
 };
